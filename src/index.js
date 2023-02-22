72f70256fbeafae5e8feab9a9f1c40c06184677c
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-02-22 13:10:28 +0530

    build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-var http = require("http");
+// var http = require("http");
 
-const httpServer = http.createServer(handleServer);
+// const httpServer = http.createServer(handleServer);
 
 
-function handleServer(req, res) {
+// function handleServer(req, res) {
   
-}
+// }
 
-module.exports = httpServer;
+// module.exports = httpServer;
+const express = require("express");
+
+const app = express();
+
+app.get("/welcome", (req, res) => {
+  res.send("Welcome to Dominos!");
+  res.sendStatus(200);
+});
+
+app.get("/contact", (req, res) => {
+  res.json({
+    phone: "18602100000",
+    email: "guestcaredominos@jublfood.com",
+  });
+  res.sendStatus(200);
+});
+
+app.listen(8081, () => {
+  console.log("Listening to port 8081");
+});
